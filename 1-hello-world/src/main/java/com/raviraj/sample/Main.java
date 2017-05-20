db73c6467d1c857f75dec69f8d8b7ee59cd8028b
Raviraj Mahamuni <mahamuniraviraj@gmail.com>
2017-05-20 22:12:59 +0000

    Updated Comment
    Added Documentation comment. Committed from github repository

--- 1-hello-world/src/main/java/com/raviraj/sample/Main.java
+++ 1-hello-world/src/main/java/com/raviraj/sample/Main.java
@@ -1,7 +1,9 @@
 package com.raviraj.sample;
 
 /**
+ * Just to Hello World
  * @author raviraj
+ * 
  *
  */
 public class Main {
@@ -9,6 +11,7 @@ public class Main {
 	public static void main(String[] args) {
 		
 			System.out.println("Hello World");
+			System.out.println("I am learning git , github and egit in eclipse.");
 
 	}
 
